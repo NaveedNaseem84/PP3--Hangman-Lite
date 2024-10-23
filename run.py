69a7bf3937fa8431a22bb91f350e9d716ac9b962
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2024-10-23 10:56:58 +0000

    add pseudo skeleton for game as comments

--- run.py
+++ run.py
@@ -8,5 +8,31 @@ def display_instructions():
     print(instructions)
     file.close()
 
+#---pseudo skeleton for game---
+
+#1. Function for random word
+# - import the words from a external file like above?
+# - randomly assign to a variable and return the value
+
+
+#2. function to take input from user
+# - validate so it is only a single letter 
+
+#3. play game function
+# - take the input from the user
+# - compare this to the selected word which has the characters replaced as _ or * till guessed
+# - if the input matches a letter in the word, replace that */_ with input
+# - if input doesn't match, reduce an attempt and let the user know the input isn't in the word
+# - If a repeat input is tried, store this to check and let user know this has already been tried.
+# - If all attempts used up, game over
+# - if word guessed, game won.
+# - run on loop till either word guessed or attempts over
+
+#4. option to play again on game won or game over.
+
+#5. option to set difficulty
+
+
 
 display_instructions()
+
