b62d58b5f17c2353d1a60d8d17a6fe30616dce28
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2024-10-23 11:32:30 +0000

    import words from file, select random word

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 def display_instructions():
     """
     Instructions for the game read in from the
@@ -7,6 +9,23 @@ def display_instructions():
     instructions = file.read()
     print(instructions)
     file.close()
+
+def choose_random_word():
+    """
+    Read in words from words file and assign to list
+    using the new line as the value seperator. Randomly
+    select and return a word from this list.
+    """
+    #source for words: 
+    # https://www.thegamegal.com/wp-content/uploads/2011/11/Pictionary-Words-Medium.pdf
+    
+    file = open("words.txt", 'r')
+    words = file.read()
+    word_list = words.split("\n")
+    file.close()
+    choose_word = random.choice(word_list)
+    print(f"Random word: {choose_word}")
+
 
 #---pseudo skeleton for game---
 
@@ -34,5 +53,6 @@ def display_instructions():
 
 
 
-display_instructions()
+#display_instructions()
+choose_random_word()
 
