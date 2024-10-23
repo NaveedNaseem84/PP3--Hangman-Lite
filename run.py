37be4c959b3b94276c800e6959f5b7ef63b7c718
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2024-10-23 10:08:23 +0000

    import and display instructions

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-def test():
-    print("Hello!")
+def display_instructions():
+    """
+    Instructions for the game read in from the
+    instructions file and displayed
+    """
+    file = open("instructions.txt", 'r')
+    instructions = file.read()
+    print(instructions)
+    file.close()
 
-test()
+
+display_instructions()
